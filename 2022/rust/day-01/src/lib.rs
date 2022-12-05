baef0175c542bfc48ab3c8668df9670719819b6d
phate45 <the.big.n00b@seznam.cz>
2022-12-05 10:02:32 +0100

    Solved 2022/Day 1 with Rust
    - simple load/parse/iter/sum problem
    - followed by a max, or a max on a subset of the sorted data

---
+++ 2022/rust/day-01/src/lib.rs
@@ -0,0 +1,48 @@
+fn part1(source: &str) -> u32 {
+    *add_load(source).iter().max().unwrap()
+}
+
+fn part2(source: &str) -> u32 {
+    let mut load = add_load(source);
+    load.sort_unstable();
+
+    load[load.len() - 3..].iter().sum()
+}
+
+fn add_load(source: &str) -> Vec<u32> {
+    source
+        .split("\n\n")
+        .map(|load| load.lines().map(|item| item.parse::<u32>().unwrap()).sum())
+        .collect()
+}
+
+#[cfg(test)]
+mod tests {
+    use std::fs;
+
+    use super::*;
+
+    #[test]
+    fn part1_test() {
+        let file = fs::read_to_string("./test_input.txt").unwrap();
+        assert_eq!(part1(&file), 24000);
+    }
+
+    #[test]
+    fn part1_real() {
+        let file = fs::read_to_string("./real_input.txt").unwrap();
+        assert_eq!(part1(&file), 74394);
+    }
+
+    #[test]
+    fn part2_test() {
+        let file = fs::read_to_string("./test_input.txt").unwrap();
+        assert_eq!(part2(&file), 45000);
+    }
+
+    #[test]
+    fn part2_real() {
+        let file = fs::read_to_string("./real_input.txt").unwrap();
+        assert_eq!(part2(&file), 212836);
+    }
+}
